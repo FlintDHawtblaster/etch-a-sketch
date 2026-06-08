77b7a1d2b57c9f7437dc8ff8a19c29f6e4a971fb
FlintJunior <137229417+FlintDHawtblaster@users.noreply.github.com>
2026-06-08 13:51:31 +0000

    Create a 16x16 grid with CSS flexbox

---
+++ javaScriptFile.js
@@ -0,0 +1,13 @@
+const containerDiv = document.querySelector(".container");
+
+for (let i = 1; i <= 16; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.id = `grid-${i}`;
+    newDiv.className = "gridClass";
+    for (let j = 1; j <= 16; j++) {
+        const innerDiv = document.createElement("div");
+        innerDiv.className = `grid-inner`;
+        newDiv.appendChild(innerDiv);
+    }
+    containerDiv.appendChild(newDiv);
+}
