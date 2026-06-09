82f300410c7d7c123b518a6047de05df180f66df
FlintJunior <137229417+FlintDHawtblaster@users.noreply.github.com>
2026-06-09 11:38:38 +0000

    Add an event listener for the hover effect

--- javaScriptFile.js
+++ javaScriptFile.js
@@ -4,10 +4,17 @@ for (let i = 1; i <= 16; i++) {
     const newDiv = document.createElement("div");
     newDiv.id = `grid-${i}`;
     newDiv.className = "gridClass";
+
     for (let j = 1; j <= 16; j++) {
         const innerDiv = document.createElement("div");
         innerDiv.className = `grid-inner`;
         newDiv.appendChild(innerDiv);
+
+        innerDiv.addEventListener("mouseenter", (event) => {
+            event.target.style.backgroundColor = "black";
+        })
     }
+
     containerDiv.appendChild(newDiv);
 }
+
