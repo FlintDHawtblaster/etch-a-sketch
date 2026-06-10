2dab1a9bdb444ca8be344346395080c5a9f28638
FlintJunior <137229417+FlintDHawtblaster@users.noreply.github.com>
2026-06-10 12:51:35 +0000

    Create logic for deleting old grid and recreating a new grid based on the user's input

--- javaScriptFile.js
+++ javaScriptFile.js
@@ -1,4 +1,31 @@
 const containerDiv = document.querySelector(".container");
+const gridBtn = document.querySelector("button");
+let btnPressed = false;
+let numberOfSquares = 0; 
+
+function createGrid(booleanPressed) {
+    if (booleanPressed) {
+        containerDiv.innerHTML = "";
+
+        for (let i = 1; i <= numberOfSquares; i++) {
+            const newDiv = document.createElement("div");
+            newDiv.id = `grid-${i}`;
+            newDiv.className = "gridClass";
+
+            for (let j = 1; j <= numberOfSquares; j++) {
+                const innerDiv = document.createElement("div");
+                innerDiv.className = `grid-inner`;
+                newDiv.appendChild(innerDiv);
+
+                innerDiv.addEventListener("mouseenter", (event) => {
+                    event.target.style.backgroundColor = "black";
+                })
+            }
+
+            containerDiv.appendChild(newDiv);
+        }
+    }
+}   
 
 for (let i = 1; i <= 16; i++) {
     const newDiv = document.createElement("div");
@@ -17,4 +44,16 @@ for (let i = 1; i <= 16; i++) {
 
     containerDiv.appendChild(newDiv);
 }
+
+gridBtn.addEventListener("click", (event) => {
+    btnPressed = true;
+    numberOfSquares = prompt("Number of squares per side for the new grid (1-100):");
+    numberOfSquares = parseInt(numberOfSquares);
+    if (numberOfSquares > 0 && numberOfSquares <101) {
+        createGrid(btnPressed);
+    } else {
+        alert("Please enter a number between 1 and 100");
+    }
+    
+})
 
